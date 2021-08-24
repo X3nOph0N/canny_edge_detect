67cef84d0683ad4ff5f10460b5a1fb2a9826c48f
Xe <1079891418@qq.com>
2021-08-25 03:41:28 +0800

    add antetype for functions

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
 from cv2 import imread,imshow,namedWindow,createTrackbar
 from cv2 import getTrackbarPos,setTrackbarPos,waitKey,imwrite
 from cv2 import destroyAllWindows
-from numpy import ndarray,array
+from numpy import int16, ndarray,array
 from scipy.signal import convolve2d
 from math import pi,exp
 
-def load_fig(file_name: str, **kwargs) -> tuple(ndarray, str):
+def load_fig(file_name: str, **kwargs) -> ndarray:
     fig = imread(filename=file_name, flags=[-1])
     return fig
 
 
 def show_figs(fig: ndarray, **kwargs) -> None:
 
+    kernel_size = kwargs['gaussian_kernel_size']
+    sigma = kwargs['gaussian_sigma']
     window_name = 'canny edge detect algorithm'
     processed_fig = fig.copy()
 
@@ -19,7 +21,8 @@ def show_figs(fig: ndarray, **kwargs) -> None:
         min_val = getTrackbarPos('min_Val', window_name)
         if min_val > getTrackbarPos('max_Val', window_name):
             setTrackbarPos('max_Val', window_name, min_val)
-        process_fig(fig,processed_fig)
+        max_val = getTrackbarPos('max_Val',window_name)
+        process_fig(fig,processed_fig,kernel_size,sigma,min_val,max_val)
         return None
 
     def on_switch_update()-> None:
@@ -48,9 +51,11 @@ def show_figs(fig: ndarray, **kwargs) -> None:
     return
 
 
-def process_fig(fig: ndarray, target: ndarray, **kwargs) -> None:
+def process_fig(fig: ndarray, target: ndarray,kernel_size:int,sigma:float,min_val:int,max_val:int, **kwargs) -> None:
     filtered_fig = gaussian_filter(fig)
-    target = filtered_fig.copy()
+    gradient_x,gradient_y = calculate_gradient(filtered_fig)
+    regressioned_fig = non_max_regression(filtered_fig,gradient_x,gradient_y)
+    target = double_threshold_process(regressioned_fig,min_val,max_val)
     return
 
 
@@ -68,10 +73,13 @@ def gaussian_filter(fig:ndarray,kernel_size:int,sigma:float)->ndarray:
     
     filtered_fig = convolve2d(fig,gaussian_kernel,mode='same',boundary='fill',fillvalue=0)
     
-    return filtered_fig
+    return filtered_fig.astype(int16)
 
-def calculate_gradient():
+def calculate_gradient(fig:ndarray)->tuple(ndarray,ndarray):
     return
 
-def non_max_regression():
+def non_max_regression(fig:ndarray,gradient_x:ndarray,gradient_y:ndarray)->ndarray:
+    return
+
+def double_threshold_process(fig:ndarray,min_val:int,max_val:int)->ndarray:
     return
